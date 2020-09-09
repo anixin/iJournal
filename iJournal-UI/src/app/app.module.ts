868cc285af030bf8a14b0285f5d0caa797443ebe
Nityansh <nityansh.ins@gmail.com>
2020-09-10 00:30:28 +0530

    adding basic UI layot

--- iJournal-UI/src/app/app.module.ts
+++ iJournal-UI/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidePanelComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ iJournal-UI/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.styl']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+  public schedule: any = [];
+
+  ngOnInit() {
+    this.schedule.push({
+      toDo: "Do Leetcode",
+      timeCommitted: '15 mins'
+    });
+  }
+
+  addTask() {
+  }
+
+}
